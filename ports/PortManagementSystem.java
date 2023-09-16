4907fa29ebb0216207f7ba1a780a6e97d41ed9ff
Vinh Gia Bao Nguyen <s3986287@rmit.edu.vn>
2023-09-16 15:17:27 +0700

    Update PMS

--- ports/PortManagementSystem.java
+++ ports/PortManagementSystem.java
@@ -8,39 +8,32 @@ import java.util.Scanner;
 
 public class PortManagementSystem {
     public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        Scanner scanner = new Scanner(System.in);
         List<Ports> portsList = Ports.readFromFile(Ports.filename);
 
         while (true) {
-            System.out.println("Please select an option:");
             System.out.println("1. Create new port");
             System.out.println("2. View all ports");
             System.out.println("3. Update existing port");
             System.out.println("4. Delete existing port");
             System.out.println("5. Exit");
-
-            String choice = reader.readLine();
+            System.out.print("Please select an option: ");
+            int choice = scanner.nextInt();
 
             switch (choice) {
-                case "1":
-                    Ports.createPort(portsList, Ports.filename);
-                    break;
-                case "2":
+                case 1 -> Ports.createPort(portsList, Ports.filename);
+                case 2 -> {
                     for (Ports port : portsList) {
                         System.out.println(port);
                     }
-                    break;
-                case "3":
-                    Ports.updatePort(portsList, Ports.filename);
-                    break;
-                case "4":
-                    Ports.deletePort(portsList, Ports.filename);
-                    break;
-                case "5":
+                }
+                case 3 -> Ports.updatePort(portsList, Ports.filename);
+                case 4 -> Ports.deletePort(portsList, Ports.filename);
+                case 5 -> {
                     System.out.println("Exiting...");
                     return;
-                default:
-                    System.out.println("Invalid choice. Please try again.");
+                }
+                default -> System.out.println("Invalid choice. Please try again.");
             }
         }
     }
